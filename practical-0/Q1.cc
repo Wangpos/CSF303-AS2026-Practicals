3b7de940277ef5f2e2f64d7ab09ddf2deb72fdca
twangpodorji <02230311.cst@rub.edu.bt>
2026-03-04 13:29:27 +0600

    feat(Q1 & Q2): answers pushed main branch

---
+++ practical-0/Q1.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+     string name = "Tsheirng Wangpo Dorji"; // name as sting
+     string stdnumber = "02230311"; // student number as integer
+     string Course = "BSc Computer Science";
+     cout << "Name: " << name << endl;
+     cout << "Student No: " << stdnumber << endl;
+     cout << "Course: " << Course << endl;
+     return 0;
+}
---
+++ practical-0/Q2.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+     int stdnumber = 2230311;
+     
+     // Calculate sum of all digits
+     int temp = stdnumber;
+     int sum = 0;
+     while (temp > 0) {
+          sum += temp % 10;
+          temp /= 10;
+     }
+     string oddEven = (stdnumber % 2 == 0) ? "Even" : "Odd";
+     
+     int remainder = stdnumber % 7;
+     
+     int multiplied = stdnumber * 3;
+     
+     cout << "Student Number: " << stdnumber << endl;
+     cout << "Sum of all digits: " << sum << endl;
+     cout << "Odd or Even: " << oddEven << endl;
+     cout << "Remainder when divided by 7: " << remainder << endl;
+     cout << "Student number multiplied by 3: " << multiplied << endl;
+
+     return 0;
+}
